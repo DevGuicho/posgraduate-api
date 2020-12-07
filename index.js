37a165471c29e00a132c25820e17776e50d45a50
Luis Vazquez <luvazpa@gmail.com>
2020-12-07 10:22:08 -0600

    service added

--- index.js
+++ index.js
@@ -3,10 +3,7 @@ const usersApi = require('./routes/users');
 const app = express();
 const { config } = require('./config');
 
-// parse requests of content-type - application/json
 app.use(express.json());
-
-// parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
 //User Routes
